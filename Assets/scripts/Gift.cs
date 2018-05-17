0a674597ca2ed479de34d64d1db55e41cb4f6ed7
Sandra Jasionowska <35003792+sjasionowska@users.noreply.github.com>
2018-05-17 14:05:42 +0200

    Level design, assets, tilemap
    Level designed and ready for next work. Some assets added, converted to tiles, platforms made with tilemap.

---
+++ Assets/scripts/Gift.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gift : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            //Gift.SetTile(new Vector3Int(-7, 3, 0), null);
+            GetComponent<AudioSource>().Play();
+            //FindObjectOfType<PointsCounter>().IncrementPoints();
+            //GetComponent<SpriteRenderer>().enabled = false;
+            //Destroy(gameObject, 3f);
+        }
+
+    }
+
+}
